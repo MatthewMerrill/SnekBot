e90a30181ecb23a152f7a1198868920ea696dbb7
Matthew Merrill <mattmerr47@gmail.com>
2019-04-01 19:40:01 -0700

    update

--- public/bot.js
+++ public/bot.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sneknet v2: Eletric Boogaloo
 // @namespace    https://sneknet.com/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Matthew Merrill <mattmerr.com>
 // @match        https://www.reddit.com/sequence
